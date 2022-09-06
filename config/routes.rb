d1aa9f2905486edfea88631f416dabf438ffd1ee
saira2025 <sairamunawar2025@gmail.com>
2022-09-06 11:13:46 +0500

    Adding home_Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index'
+  root to: "home#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
