d53a3c893555854bcb600037fa1b590272adda71
saira2025 <sairamunawar2025@gmail.com>
2022-09-07 12:35:08 +0500

    applying user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Constants
+  PASSWORD_FORMAT = /\A
+  (?=.{8,})          # Must contain 8 or more characters
+  (?=.*[A-Z])        # Must contain an upper case character
+  (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x.freeze
+
+  # Enums
   enum role: %i[artist manager]
+
+  # Validations
+  validates :email, :password, :role, presence: true
+  validates :password, format: { with: PASSWORD_FORMAT }
 end
