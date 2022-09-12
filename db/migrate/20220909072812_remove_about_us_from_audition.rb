0291444de8bd44a7584727bd241eafa6efb6c37c
saira2025 <sairamunawar2025@gmail.com>
2022-09-12 19:14:04 +0500

    Adding Addition link in artist portal

--- db/migrate/20220909072812_remove_about_us_from_audition.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# remove about os from audition
-class RemoveAboutUsFromAudition < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :auditions, :about_us, :integer
-  end
-end
